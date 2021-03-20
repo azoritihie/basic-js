bb8cf0900357368df85c0dfac48b1b7127c5ca09
azoritihie <butterfly140611@gmail.com>
2021-03-20 09:46:34 +0400

    feat: add solution dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,29 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam(team) {
+  //throw new CustomError('Not implemented');
+  if (!Array.isArray(team)){
+    return false
+  }
+    let count = 0;
+    let result = '';
+      for (let i = 0; i < team.length; i ++) {
+      //for (let j = 0; j <team[i].length; j++){
+        if (typeof team[i] === 'string'){
+          count =+ 1;
+        }}
+    
+        if (count === 0){
+          return false;}
+        else {
+    for (let i = 0; i < team.length; i ++) {
+         if (typeof team[i] === 'string'){
+          team[i] = team[i].trim().toUpperCase();
+          if (team[i][0].toUpperCase() === team[i][0]) {
+              result = result + team[i][0];
+      }
+    }
+    }
+    return result.split('').sort().join('');    
+    }
+  }
